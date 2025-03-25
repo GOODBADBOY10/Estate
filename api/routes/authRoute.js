f5d33fa7b6301ba4f6fd8a159599729624416b21
GOODBADBOY10 <selfmanademola@gmail.com>
2025-03-25 21:38:23 +0100

    create a register, login and logout route

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -7,5 +7,9 @@ router.get('/test', (req, res) => {
     console.log('router works')
 })
 
+router.get('/register', (req, res) => {})
+router.get('/login', (req, res) => {})
+router.get('/logout', (req, res) => {})
+
 
 export default router
