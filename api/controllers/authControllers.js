b356f40055537d74f148447f67badb3caafc3fb8
GOODBADBOY10 <selfmanademola@gmail.com>
2025-03-25 22:37:38 +0100

    connect to a database using prisma and mongodb.I also created the register route and test it

--- api/controllers/authControllers.js
+++ api/controllers/authControllers.js
@@ -1,5 +1,20 @@
-export const register = (req, res) => {
-    // database operations
+import bcrypt from 'bcrypt';
+import prisma from '../lib/prisma.js';
+
+export const register = async (req, res) => {
+    const { username, email, password } = req.body
+
+    //Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    //create a new user 
+    const newUser = await prisma.user.create({
+        data: {
+            username,
+            email,
+            password: hashedPassword,
+        }
+    });
+    console.log(newUser);
 }
 
 
--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { login, logout, register } from '../controllers/authControllers';
+import { login, logout, register } from '../controllers/authControllers.js';
 
 const router = express.Router();
 
